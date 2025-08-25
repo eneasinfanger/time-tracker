193a5d4f369d857b61f0de3408f001b200d2273c
Eneas Infanger <eneas.infanger@loewenfels.ch>
2025-08-25 08:08:07 +0200

    Install tailwind as vite plugin instead of using cdn

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite"
 import { viteSingleFile } from "vite-plugin-singlefile"
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [viteSingleFile()],
+    plugins: [
+        viteSingleFile(),
+        tailwindcss(),
+    ],
 })
