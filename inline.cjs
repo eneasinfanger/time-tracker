af4391ddb1495369afacaacfec36d96b64393adf
Eneas Infanger <eneasinfanger@icloud.com>
2025-11-04 23:13:40 +0100

    add favicon and inline links

--- inline.cjs
+++ inline.cjs
@@ -3,64 +3,94 @@ const fs = require('fs');
 const path = require('path');
 const esbuild = require('esbuild');
 
-const distPath = path.join(__dirname, 'dist/time-tracker/browser');
-const indexPath = path.join(distPath, 'index.html');
-const outputHtmlPath = path.join(distPath, 'index.html');
-
 (async () => {
   try {
-    const mainFile = path.join(distPath, '*.js');
+    const distPath = path.join(__dirname, 'dist/time-tracker/browser');
+    const indexPath = path.join(distPath, 'index.html');
 
-    await esbuild.build({
-      entryPoints: [mainFile],
-      bundle: true,
-      format: 'iife',
-      platform: 'browser',
-      outdir: distPath,
-      allowOverwrite: true,
-      sourcemap: false,
-      minify: true
-    });
+    await bundleJs(distPath);
 
     const html = fs.readFileSync(indexPath, 'utf8');
 
-    inline.html(
-      {
-        fileContent: html,
-        relativeTo: distPath
-      },
-      (err, inlinedHtml) => {
-        if (err) {
-          throw err;
-        }
+    inlineImages(distPath, html, (err, inlinedHtml) => {
+      if (err) { throw err; }
 
-        const faviconRegex = /<link\b[^>]*\brel=["']icon["'][^>]*\btype=["']([^"']+)["'][^>]*\bhref=["']([^"']+)["'][^>]*>/i;
-        const match = inlinedHtml.match(faviconRegex);
-        if (!match) {
-          console.warn('No favicon link found in HTML.');
-        } else {
-          const mimeType = match[1];
-          const imagePath = path.isAbsolute(match[2]) ? match[2] : path.join(distPath, match[2]);
+      inlinedHtml = inlineLinks(distPath, inlinedHtml);
 
-          const imageBuffer = fs.readFileSync(imagePath);
-          const base64Image = imageBuffer.toString('base64');
-          const dataUri = `data:${ mimeType };base64,${ base64Image }`;
+      fs.writeFileSync(indexPath, inlinedHtml, 'utf8');
+      console.log(`All resources inlined → ${ indexPath }`);
 
-          inlinedHtml = inlinedHtml.replace(faviconRegex, `<link rel="icon" type="${ mimeType }" href="${ dataUri }">`);
-        }
-
-        fs.writeFileSync(outputHtmlPath, inlinedHtml, 'utf8');
-        console.log(`All resources inlined → ${ outputHtmlPath }`);
-
-        fs.readdirSync(distPath).forEach(file => {
-          if (file !== 'index.html') {
-            const fullPath = path.join(distPath, file);
-            fs.rmSync(fullPath, { recursive: true, force: true });
-          }
-        });
-      }
-    );
+      removeOtherFiles(distPath, ['index.html']);
+    });
   } catch (err) {
     console.error('Error inlining:', err);
   }
 })();
+
+async function bundleJs(distPath) {
+  await esbuild.build({
+    entryPoints: [path.join(distPath, '*.js')],
+    bundle: true,
+    format: 'iife',
+    platform: 'browser',
+    outdir: distPath,
+    allowOverwrite: true,
+    sourcemap: false,
+    minify: true
+  });
+}
+
+function inlineImages(distPath, html, onComplete) {
+  inline.html({ fileContent: html, relativeTo: distPath }, onComplete);
+}
+
+function inlineLinks(distPath, inlinedHtml) {
+  const linkRegex = /<link\b(?=[^>]*\brel=["'][\w-]+["'])((?:(?!\btype=|\bhref=)[^>])*)(?:\btype=["']([^"']+)["'])?((?:(?!\bhref=)[^>])*)(?:\bhref=["']([^"']+)["'])?([^>]*)>/ig;
+
+  return inlinedHtml.replaceAll(linkRegex, (match, attrBefore, type, attrBetween, href, attrAfter) => {
+    const resourcePath = path.isAbsolute(href) ? href : path.join(distPath, href);
+
+    if (!fs.existsSync(resourcePath) || !fs.statSync(resourcePath).isFile()) {
+      console.warn(`Warning: Resource not found for inlining: ${ resourcePath }`);
+      return match;
+    }
+
+    const fileBuffer = fs.readFileSync(resourcePath);
+    const base64Data = fileBuffer.toString('base64');
+    const dataUri = `data:${ type ?? '' };base64,${ base64Data }`;
+
+    const padStart = s => ' ' + s.trim();
+    const padEnd = s => s.trim() + ' ';
+    return strBuilder()
+      .append('<link ')
+      .append(attrBefore, padEnd)
+      .append(type, t => `type=${ t } `)
+      .append(attrBetween, padEnd)
+      .append(`href="${ dataUri }"`)
+      .append(attrAfter, padStart)
+      .append('>')
+      .toString();
+  });
+}
+
+function strBuilder() {
+  return {
+    data: '',
+    append(s, f) {
+      if (s) {
+        if (f) { this.data += f(s); }
+        else { this.data += s; }
+      }
+      return this;
+    },
+    toString() { return this.data; }
+  };
+}
+
+function removeOtherFiles(distPath, excludes) {
+  fs.readdirSync(distPath).forEach(file => {
+    if (!excludes.includes(file)) {
+      fs.rmSync(path.join(distPath, file), { recursive: true, force: true });
+    }
+  });
+}
